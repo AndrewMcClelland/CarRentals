06d4b894f9f3421912a8263813c5931619b010a9
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2017-03-12 13:08:19 -0400

    Removed part 1 folder, Project Outline pdf, and updated gitignore.

--- Part I/MySQL Dumps/car_rental_history_SQL.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE car_rental_history (
-  VIN int(11) NOT NULL,
-  MemberID int(11) NOT NULL,
-  PickupOdometer int(11) NOT NULL,
-  DropoffOdometer int(11) NOT NULL,
-  Status varchar(11) NOT NULL,
-  Date date NOT NULL
-  primary key (VIN, MemberID, PickupOdometer, DropoffOdometer, Status, Date));
--- Part I/MySQL Dumps/rental_comment_SQL.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE rental_comment (
-  VIN int(11) NOT NULL,
-  MemberID int(11) NOT NULL,
-  Rating int(11) NOT NULL,
-  Comment varchar(500) NOT NULL,
-  Date date NOT NULL,
-  ReplyComment varchar(500) NOT NULL,
-  ReplyDate date NOT NULL,
-  primary key (VIN, MemberID, Rating, Comment, Date, ReplyComment, ReplyDate));
