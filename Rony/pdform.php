3b69d22cd8f7938c729bde16e809aef54b074016
Rony Besprozvanny <besprozvanny@gmail.com>
2017-03-21 11:50:13 -0400

    Continuing to work on getting the form to dynamically change based on objects state

--- Rony/pdform.php
+++ Rony/pdform.php
@@ -10,27 +10,94 @@
 </head>
 <body>
 
-<div class="container-fluid">
-<h1>Pickup Tesla Model S</h1>
-<p>Please complete the following form to obtain your keys</p>
-</div>
+  <?php
 
-<form action="pdhandle.php" method="post">
-<div class="form-group">
-<label for="pickupOdoVal">Current Odometer Reading:</label>
-<input type="text" name="curOdoReading" class="form-control" placeholder="Must be numeric entry" id="pickupOdoVal">
-</div>
+      $host = "localhost";
+      $user = "root";
+      $password = "";
+      $database = "KTCS";
 
-<div class="form-group">
-<label for="carStat"> Car Status </label>
-<input type="text" class="form-control" name="carStatus" id="carStat" >
-<span class="help-block">Status can either be Normal, Damaged or Repair</span>
-</div>
-<button type="submit" class="btn btn-default">Submit</button>
-</form>
+      $cxn = mysqli_connect($host,$user,$password, $database);
+
+      if (mysqli_connect_errno()) {
+          echo "Failed to connect to MySQL: " . mysqli_connect_error();
+          die();
+      }
 
 
+      $memberID = "20";
+      $VIN = "20140294";
 
+      // Check to see if the object exists
+      $sqlQuery = "select *
+                   from Car_Rental_History
+                   where Car_Rental_History.VIN=$VIN and Car_Rental_History.MemberID=$memberID";
 
+      $resultVal = mysqli_query($cxn, $sqlQuery);
+      if (mysqli_num_rows($resultVal) > 0)
+      {
+        echo "Row exists<br></br>";
+        // output data of each row
+    		while($row = mysqli_fetch_assoc($resultVal))
+        {
+    			echo "VIN: " . $row["VIN"].
+          "<br>MemberID: " . $row["MemberID"].
+          "<br>Pickup Odometer: " . $row["PickupOdometer"].
+          "<br>Pickup Status: " . $row["PickupStatus"].
+          "<br>Pickup Date: " . $row["PickupDate"].
+          "<br>Dropoff Odometer: " . $row["DropoffOdometer"].
+          "<br>Dropoff Status: " .$row["DropoffStatus"].
+          "<br>Dropoff Date: " .$row["DropoffDate"].
+          "<br></br>";
+
+          // If object exists, assumed pickup values set - check to see if dropoff is set
+          // echo "DROPOFF ODOMETER NOT SET";
+          //echo "<br></br>Show Dropoff Form, not Pickup Form";
+          if($row["DropoffOdometer"] == 0 || $row["DropoffOdometer"] == NULL)
+          { ?>
+            <div class="container-fluid">
+            <h1>Dropoff Tesla Model S</h1>
+            <p>Please complete the following form before dropping off your keys</p>
+            </div>
+
+            <form action="pdhandle.php" method="post">
+            <div class="form-group">
+            <label for="dropoffOdoVal">Current Odometer Reading:</label>
+            <input type="text" name="curOdoReading" class="form-control" placeholder="Must be numeric entry" id="dropoffOdoVal">
+            </div>
+
+            <div class="form-group">
+            <label for="carStat"> Car Status </label>
+            <input type="text" class="form-control" placeholder="Enter car status" name="carStatus" id="carStat" >
+            <span class="help-block">Status can either be Normal, Damaged or Repair</span>
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+          <?php
+          }
+        }
+      }
+      else
+      { ?>
+        <div class="container-fluid">
+        <h1>Pickup Tesla Model S</h1>
+        <p>Please complete the following form to obtain your keys</p>
+        </div>
+
+        <form action="pdhandle.php" method="post">
+        <div class="form-group">
+        <label for="pickupOdoVal">Current Odometer Reading:</label>
+        <input type="text" name="curOdoReading" class="form-control" placeholder="Must be numeric entry" id="pickupOdoVal">
+        </div>
+
+        <div class="form-group">
+        <label for="carStat"> Car Status </label>
+        <input type="text" class="form-control" placeholder="Enter car status" name="carStatus" id="carStat" >
+        <span class="help-block">Status can either be Normal, Damaged or Repair</span>
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+      <?php }
+      ?>
 </body>
 </html>
--- Rony/pdhandle.php
+++ Rony/pdhandle.php
@@ -15,20 +15,20 @@
 </div>
 
 <?php
-    
+
     $host = "localhost";
     $user = "root";
     $password = "";
     $database = "KTCS";
-    
+
     $cxn = mysqli_connect($host,$user,$password, $database);
-    
+
     if (mysqli_connect_errno()) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
         die();
     }
-    
-    
+
+
     $memberID = "20";
     $VIN = "20140294";
     $pickupOdo = $_POST["curOdoReading"];
@@ -36,9 +36,51 @@
     date_default_timezone_set('US/Eastern');
     $currentDate = date('Y-m-d H:i:s');
 
-    mysqli_query($cxn, "insert into Car_Rental_History values
+    // Before executing the car insertion check to see if the object exists
+    $sqlQuery = "select *
+                 from Car_Rental_History
+                 where Car_Rental_History.VIN=$VIN and Car_Rental_History.MemberID=$memberID";
+
+    $resultVal = mysqli_query($cxn, $sqlQuery);
+    if (mysqli_num_rows($resultVal) > 0)
+    {
+      echo "Row exists<br></br>";
+      // output data of each row
+  		while($row = mysqli_fetch_assoc($resultVal))
+      {
+  			echo "VIN: " . $row["VIN"].
+        "<br>MemberID: " . $row["MemberID"].
+        "<br>Pickup Odometer: " . $row["PickupOdometer"].
+        "<br>Pickup Status: " . $row["PickupStatus"].
+        "<br>Pickup Date: " . $row["PickupDate"].
+        "<br>Dropoff Odometer: " . $row["DropoffOdometer"].
+        "<br>Dropoff Status: " .$row["DropoffStatus"].
+        "<br>Dropoff Date: " .$row["DropoffDate"].
+        "<br></br>";
+        if($row["DropoffOdometer"] == 0 || $row["DropoffOdometer"] == NULL)
+        {
+          echo "Fill up the drop off values";
+          $sqlQuery = "UPDATE Car_Rental_History
+                      SET `DropoffOdometer`=$pickupOdo
+                      WHERE MemberID=$memberID and VIN=$VIN";
+          mysqli_query($cxn, $sqlQuery);
+        }
+        else
+        {
+          echo "Can't update dropoff values again - this can only be done once";
+          echo "<br></br>Please consult admin if you made an error in the form";
+        }
+  		}
+    }
+    else
+    {
+      echo "Row does not exist - Creating object";
+      echo "Fill with pickup values";
+      // Create the pickup object
+      mysqli_query($cxn, "insert into Car_Rental_History values
                  ('$VIN', '$memberID', '$pickupOdo', 'NULL', '$pickupStatus', 'NULL', '$currentDate', 'NULL')
                  ;");
+    }
     ?>
     <!-- Need to ensure object is created before showing this -->
     <h4> Keys are now dispensing </h4>
