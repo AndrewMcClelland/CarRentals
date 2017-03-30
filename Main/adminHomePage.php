ba5b80bc94585b858ba29dce9e6b1d15fe8139a5
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2017-03-30 17:09:46 -0400

    Linked Rony's monthly invoice page to the admin homepage and added some styling.

--- Main/adminHomePage.php
+++ Main/adminHomePage.php
@@ -14,16 +14,91 @@
 <h1>Admin Homepage</h1>
 </div>
 
+<?php
+
+      $host = "localhost";
+      $user = "root";
+      $password = "";
+      $database = "KTCS";
+
+      $cxn = mysqli_connect($host,$user,$password, $database);
+
+      if (mysqli_connect_errno()) {
+          echo "Failed to connect to MySQL: " . mysqli_connect_error();
+          die();
+      }
+
+
+      //$memberID = "38";
+      //$VIN = "20140294";
+
+      // Will only be able to access this form if logged in as admin
+
+      // Trick here is we want to be able to see the list of members
+      // Query the whole member list and return memberID's
+      $sqlQuery = "select *
+                   from KTCS_Member";
+
+      $resultVal = mysqli_query($cxn, $sqlQuery);
+      $memberIDArry = array();
+      $nameFirstArray = array();
+      while($row = mysqli_fetch_assoc($resultVal))
+      {
+        array_push($memberIDArry, $row["MemberID"]);
+        array_push($nameFirstArray, $row["NameFirst"]);
+      }
+      // print_r($memberIDArry);
+      // print_r($nameFirstArray);
+      ?>
+        <div class="container-fluid">
+        <h3>Monthly Inovice</h3>
+        <p>Please complete the following fields to obtain the monthly invoice of a given member</p>
+        </div>
+
+        <form action="monthlyInvoiceHandle.php" method="post">
+        <div class="form-group">
+        <label for="memberIDVal">Member ID:</label>
+        <select class="form-control" id="dopdownMember" name="memID" >
+          <?php
+          $curIndex = -1;
+          foreach($nameFirstArray as $curName) {
+            $curIndex = $curIndex + 1;
+            echo $curIndex;?>
+          <option value="<?php echo $memberIDArry[$curIndex]; ?>" ><?php echo $curName?></option>
+          <?php } ?>
+        </select>
+        <!--<input type="text" name="memID" class="form-control" placeholder="Must be numeric entry" id="memberIDVal">-->
+        </div>
+
+        <div class="form-group">
+        <label for="monthStat"> Start Invoice Month: </label>
+        <input type="date" name="monthStat" value="<?php echo date('Y-m-d'); ?>" />
+        </div>
+
+        <div class="form-group">
+        <label for="endMonthStat"> End Invoice Month: </label>
+        <input type="date" name="endMonthStat" value="<?php echo date('Y-m-d'); ?>" />
+        </div>
+
+        <button type="submit" class="btn btn-default">Submit</button>
+</form>
+
+<br/><hr><br/>
+
 <!-- Add a car -->
+<h3>Add Car to Fleet  </h3>
+<p>Please click the button and complete the form on the following page to add a car to the fleet.</p>
 <form action="addCarForm.php" method="post">
 	
 	<button type="submit" class="btn btn-default">Add Car to Fleet</button>
 	
 </form>
 
-<br/><br/>
+<br/><hr><br/>
 
 <!-- See car rental history -->
+<h3>View Rental History for Cars</h3>
+<p>Please select a car to view the car's rental history.</p>
 <form action="carRentalHistoryHandle.php" method="post">
 	
 	<!-- Dropdown table for KTCS cars -->
@@ -33,18 +108,6 @@
 	
 	<?php
     
-		$host = "localhost";
-		$user = "root";
-		$password = "";
-		$database = "KTCS";
-		
-		$cxn = mysqli_connect($host,$user,$password, $database);
-		
-		if (mysqli_connect_errno()) {
-			echo "Failed to connect to MySQL: " . mysqli_connect_error();
-			die();
-		}
-
 		$sql_cars_dropdown = "	SELECT vin, make, model, year, locationid, colour, picturelink, rentalfee
 												FROM car";
 														
@@ -59,35 +122,25 @@
 	?>
 	
 	</SELECT>
-	<br/><br/>
+<br/><br/>
 	
 	<button type="submit" class="btn btn-default">View Car's Rental History</button>
 	
 </form>
 
-<br/><br/>
+<br/><hr><br/>
 
+<h3>Available Cars at Location</h3>
+ <p>View available cars at given location and reservations for each car (if any):</p>
 <form action="availableCarsAtLocation.php" method="post">
 
 	<!-- Dropdown table for KTCS Locations to show available cars -->
-	<label for="LocationID">View available cars at given location and reservations for each car (if any):</label>
+	<label for="LocationID"></label>
 	<br/>
 	<SELECT NAME = "location_dropdown">
     <option value="">Please select location...</option>
 	
 	<?php
-    
-		$host = "localhost";
-		$user = "root";
-		$password = "";
-		$database = "KTCS";
-		
-		$cxn = mysqli_connect($host,$user,$password, $database);
-		
-		if (mysqli_connect_errno()) {
-			echo "Failed to connect to MySQL: " . mysqli_connect_error();
-			die();
-		}
 
 		$sql_location_dropdown = "	SELECT LocationID, AddressLine, PostalCode, Province, City, Country, Spaces
 														FROM Parking_Location";
@@ -107,9 +160,11 @@
 	<button type="submit" class="btn btn-default">Submit</button>
 </form>
 
-<br/><br/>
+<br/><hr><br/>
 
 <!--View cars that travelled > distance since last maintenance -->
+<h3>Cars Since Maintenance</h3>
+<p>Enter a distance and view all cars that have travelled that distance since last maintenance.</p>
 <form action="carsDistanceSinceMaintenanceHandle.php" method="post">
 	
 	<div class="form-group">
