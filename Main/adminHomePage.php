cfa42710d3782bc918f8d7002e4deffa791f3c20
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2017-04-01 11:39:55 -0400

    Added rental comment functionality to adminHomepage, removed rentalComment.php

--- Main/adminHomePage.php
+++ Main/adminHomePage.php
@@ -331,5 +331,68 @@
 
 </form>
 
+<br/><hr><br/>
+
+    <?php
+    //echo $_SESSION['login_user'];
+    //echo $_SESSION["memberID"];
+
+      $host = "localhost";
+      $user = "root";
+      $password = "";
+      $database = "KTCS";
+      $cxn = mysqli_connect($host,$user,$password, $database);
+
+      if (mysqli_connect_errno()) {
+          echo "Failed to connect to MySQL: " . mysqli_connect_error();
+          die();
+      }
+
+
+      //$memberID = "38";
+      //$VIN = "20140294";
+
+      // Will only be able to access this form if logged in as admin
+
+      // Trick here is we want to be able to see the list of members
+      // Query the whole member list and return memberID's
+      $sqlQuery = "select *
+                   from KTCS_Member";
+
+      $resultVal = mysqli_query($cxn, $sqlQuery);
+      $memberIDArry = array();
+      $nameFirstArray = array();
+      while($row = mysqli_fetch_assoc($resultVal))
+      {
+        array_push($memberIDArry, $row["MemberID"]);
+        array_push($nameFirstArray, $row["NameFirst"]);
+      }
+      // print_r($memberIDArry);
+      // print_r($nameFirstArray);
+      ?>
+        <div class="container-fluid">
+        <h1>View Rental Comments</h1>
+        <p>Please select the member to see if they have left any comments on a rental</p>
+        </div>
+
+        <form action="rentalCommentHandle.php" method="post">
+        <div class="form-group">
+        <label for="memberIDVal">Member:</label>
+        <select class="form-control" id="dopdownMember" name="memID" >
+          <?php
+          $curIndex = -1;
+          foreach($nameFirstArray as $curName) {
+            $curIndex = $curIndex + 1;
+            echo $curIndex;?>
+          <option value="<?php echo $memberIDArry[$curIndex];?>" ><?php echo $curName?></option>
+          <?php } ?>
+        </select>
+        </div>
+
+        <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+		
+	<br>
+
 </body>
 </html>
